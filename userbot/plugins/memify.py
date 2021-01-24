c18c0f9d1a4dfc516ad938abf037562a7e034a1a
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-24 19:06:38 +0530

    Add files via upload

---
+++ userbot/plugins/memify.py
@@ -0,0 +1,280 @@
+
+
+
+    # Fully Modified By @danish_00
+    # 
+    # Team Cobra
+    # Dark
+
+
+
+import cv2
+import os, io,random, shutil, re ,textwrap
+import lottie
+from PIL import Image, ImageDraw, ImageEnhance, ImageFont, ImageOps
+from userbot.utils import admin_cmd
+
+path = "./dcobra/"
+if not os.path.isdir(path):
+    os.makedirs(path)
+    
+    
+@bot.on(admin_cmd(outgoing=True, pattern="mmf ?(.*)"))
+async def mim(event):
+    if not event.reply_to_msg_id:
+        await event.edit(
+            "`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' "
+        )
+        return
+    reply = await event.get_reply_message()
+    imgs = await bot.download_media(reply.media, path)
+    img = cv2.VideoCapture(imgs) 
+    tal, cobra = img.read()
+    cv2.imwrite("danish.webp", cobra)
+    await event.edit(
+        "```Memefying 🔸🔸🔸```"
+    )
+    text = event.pattern_match.group(1)
+    webp_file = await draw_meme_text("danish.webp", text)
+    await event.client.send_file(
+        event.chat_id, webp_file, reply_to=event.reply_to_msg_id
+    )
+    await event.delete()
+    shutil.rmtree(path)
+    os.remove("danish.webp")
+    os.remove(webp_file)
+
+
+async def draw_meme_text(image_path, text):
+    img = Image.open(image_path)
+    os.remove(image_path)
+    i_width, i_height = img.size
+    m_font = ImageFont.truetype(
+        "userbot/helpers/styles/impact.ttf", int((70 / 640) * i_width)
+    )
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ""
+    draw = ImageDraw.Draw(img)
+    current_h, pad = 10, 5
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            u_width, u_height = draw.textsize(u_text, font=m_font)
+
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 1, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 1, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 1),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 1),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) - 1,
+                    i_height - u_height - int((80 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) + 1,
+                    i_height - u_height - int((80 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((80 / 640) * i_width)) - 1,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((80 / 640) * i_width)) + 1,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    i_height - u_height - int((80 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+
+    image_name = "cobr.webp"
+    img.save(image_name, "WebP")
+    return image_name
+
+@bot.on(admin_cmd(outgoing=True, pattern="mms ?(.*)"))
+async def mim(event):
+    if not event.reply_to_msg_id:
+        await event.edit(
+            "`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' "
+        )
+        return
+    reply = await event.get_reply_message()
+    imgs = await bot.download_media(reply.media, path)
+    img = cv2.VideoCapture(imgs) 
+    tal, cobra = img.read()
+    cv2.imwrite("danish.webp", cobra)
+    await event.edit(
+        "```Memifying 🔸🔸🔸 ```"
+    )
+    text = event.pattern_match.group(1)
+    photo = await draw_meme("danish.webp", text)
+    await event.client.send_file(
+        event.chat_id, photo, reply_to=event.reply_to_msg_id
+    )
+    await event.delete()
+    shutil.rmtree(path)
+    os.remove("danish.webp")
+    os.remove(photo)
+
+
+async def draw_meme(image_path, text):
+    img = Image.open(image_path)
+    os.remove(image_path)
+    i_width, i_height = img.size
+    m_font = ImageFont.truetype(
+        "userbot/helpers/styles/impact.ttf", int((70 / 640) * i_width)
+    )
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ""
+    draw = ImageDraw.Draw(img)
+    current_h, pad = 10, 5
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            u_width, u_height = draw.textsize(u_text, font=m_font)
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 1, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 1, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 1),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 1),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) - 1,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) + 1,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) - 1,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) + 1,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+
+    photu = "dark.png"
+    img.save(photu, "png")
+    return photu
