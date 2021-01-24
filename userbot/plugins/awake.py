5a6d0f2ef8c9b13dce4d34b08fbab20bc601e568
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-24 18:16:33 +0530

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -19,19 +19,19 @@ from platform import python_version, uname
 
 ALIVE_PIC = Config.ALIVE_PHOTTO
 if ALIVE_PIC is None:
-   ALIVE_PIC = "https://telegra.ph/file/8458f16bebf2b7f73baa8.jpg"
+   ALIVE_PIC = "https://telegra.ph/file/9e70e26ff29442cc96249.jpg"
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 ALIVE_MESSAGE = Config.ALIVE_MSG
 if ALIVE_MESSAGE is None:
-   ALIVE_MESSAGE = "**🔱DARK COBRA IS Awake🔱 \n\n\n**"
+   ALIVE_MESSAGE = "**�𝐃𝐀𝐑𝐊 𝐒𝐇𝐀𝐃𝐎𝐖 IS Awake🔱 \n\n\n**"
    ALIVE_MESSAGE += "`My Bot Status \n\n\n`"
    ALIVE_MESSAGE += f"`Telethon: TELETHON-1.19.0 \n\n`"
    ALIVE_MESSAGE += f"`Python: PYTHON-3.8.5 \n\n`"
    ALIVE_MESSAGE += "`I'll Be With You Master Till My Dyno Ends!!☠ \n\n`"
-   ALIVE_MESSAGE += f"`Support Channel` : @Dark_cobra_support \n\n"
+   ALIVE_MESSAGE += f"`Support Channel` : @Dark_shadow_support \n\n"
    ALIVE_MESSAGE += f"`MY BOSS🤗`: {DEFAULTUSER} \n\n "
                 
             
